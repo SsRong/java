9e363bf452f017ca1cfbd1fef763dab5f4d3d408
zeyang <zeyang.ma@xiaoi.com>
2019-07-18 10:14:06 +0800

    1233444

--- src/helloworld/HellossR.java
+++ src/helloworld/HellossR.java
@@ -5,7 +5,6 @@ public class HellossR {
 	public static void main(String[] args) {
 		System.out.print("hello i am ssR");
 		System.out.println("1231213213");
-		System.out.println("1231213213");
 	}
 
 }
